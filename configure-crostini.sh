cc94ed4ffbf527d8a23dc0219847c586253abf8c
anderpups <anderpups@gmail.com>
2022-11-10 21:50:32 -0500

    Update configure-crostini.sh

--- configure-crostini.sh
+++ configure-crostini.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-## Doesn't work yet but use as a reference
+## Doesn't work yet but
+# use as a reference
 set -e
 # setup github cli repo
 if ! command -v gh &> /dev/null; then
@@ -7,6 +8,13 @@ if ! command -v gh &> /dev/null; then
     sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 fi
+
+## vscode
+sudo apt-get install wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
 
 ## update upgrade
 sudo apt update
