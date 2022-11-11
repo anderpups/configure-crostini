db3bf50a7e5f2740a2f2de13e5697e338aa08adc
anderpups <anderpups@gmail.com>
2022-11-10 21:54:18 -0500

    Update configure-crostini.sh

--- configure-crostini.sh
+++ configure-crostini.sh
@@ -10,18 +10,20 @@ if ! command -v gh &> /dev/null; then
 fi
 
 ## vscode
-sudo apt-get install wget gpg
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
-rm -f packages.microsoft.gpg
+if ! command -v code &> /dev/null; then
+    sudo apt-get install wget gpg
+    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+    sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+    sudo sh -c 'echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+    rm -f packages.microsoft.gpg
+fi
 
 ## update upgrade
 sudo apt update
 sudo apt -y dist-upgrade
 
 # install apt packages
-sudo apt -y install zsh python3-pip fonts-powerline tldr adapta-gtk-theme dconf-editor gh
+sudo apt -y install zsh python3-pip fonts-powerline tldr adapta-gtk-theme dconf-editor gh code
 
 git config --global user.email "anderpups@gmail.com"
 git config --global user.name "Brad Anderson"
